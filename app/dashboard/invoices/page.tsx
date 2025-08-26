968b0386b0c30858b9f49c600345dabf2714687b
AWALUDDIN <105841121823@student.unismuh.ac.id>
2025-08-26 20:12:46 +0800

    Initial commit

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -10,12 +10,8 @@ import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
 export default async function Page({
+  // @ts-ignore
   searchParams,
-}: {
-  searchParams?: {
-    query?: string;
-    page?: string;
-  };
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
